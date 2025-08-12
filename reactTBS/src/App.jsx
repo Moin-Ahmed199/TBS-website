aa26acc91ec2305edb7700a63451402f72abda8f
Moin Ahmed <moinahmedgh199@gmail.com>
2025-08-12 20:05:09 +0500

    first commit

---
+++ reactTBS/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css'
+
+import Navbar from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+     
+     <Navbar/>
+     
+
+
+    </>
+  )
+}
+
+export default App
---
+++ reactTBS/src/Components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+      <a
+        href="index.html"
+        className="navbar-brand d-flex align-items-center px-4 px-lg-5 mx-sm-0 btn-color navbar-margin"
+      >
+        <img
+          src=""
+          alt="Logo"
+          style={{ marginRight: "10px" }}
+          className="img-design"
+        />
+        <h2 className="m-0 text-white" id="main-heading">
+          The Brighten Stars Academy
+        </h2>
+      </a>
+
+      <button
+        type="button"
+        className="navbar-toggler me-4"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav ms-auto p-4 p-lg-0">
+          <a href="index.html" className="nav-item nav-link active">
+            Home
+          </a>
+          <a href="about.html" className="nav-item nav-link">
+            About
+          </a>
+          <a href="courses.html" className="nav-item nav-link">
+            Courses
+          </a>
+          <a href="contact.html" className="nav-item nav-link">
+            Contact
+          </a>
+        </div>
+
+        <a
+          href="Joinnow.html"
+          className="btn btn-color py-4 px-lg-5 d-none d-lg-block"
+        >
+          Join Now <i className="bi bi-arrow-right ms-3"></i>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+
+export default Navbar;
